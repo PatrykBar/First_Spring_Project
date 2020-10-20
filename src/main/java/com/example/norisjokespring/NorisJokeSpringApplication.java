e2e19084bea69407f288e95617537182a2799b43
PatBartn <patbartn9525@gmail.com>
2020-10-20 21:24:57 +0200

    First Commit

---
+++ src/main/java/com/example/norisjokespring/NorisJokeSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.norisjokespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorisJokeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorisJokeSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/norisjokespring/NorisJokeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.norisjokespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorisJokeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
